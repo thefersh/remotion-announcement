d1d77b247e3cdc79d31dad630f2c5ed8ac7d5022
Fernando Ticona <thefersh24@gmail.com>
2022-03-22 22:42:47 -0400

    spotify component

--- src/Composition.tsx
+++ src/Composition.tsx
@@ -1,11 +1,13 @@
 import { Player } from './components/player'
 import { ArtistLogo } from './components/artist'
+import { Spotify } from './components/spotify'
 import "./styles.css"
 
 export const MainScene = () => {
 	return (
 		<div className="main">
 			<ArtistLogo />
+			<Spotify />
 			<Player />
 		</div>
 	);
---
+++ src/components/spotify.tsx
@@ -0,0 +1,10 @@
+import { Img } from "remotion"
+import SpotifyImg from "./../assets/spotify.png"
+
+export const Spotify = () => {
+	return (
+		<div className="spotify">
+			<Img className="spotify__img" src={SpotifyImg} />
+		</div>
+)
+}
